015b6783f6e77eae466619ceda7f2fcee9fc609d
Joham Jaider Morales Franco <johamfranco318@gmail.com>
2023-08-21 18:13:28 -0500

    add structure for information

---
+++ src/routes/information/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+    import Header from "../../components/Header.svelte";
+    import Footer from "../../components/Footer.svelte";
+
+    import Input from "../../components/Input.svelte";
+    import ButtonSocial from "../../components/ButtonSocial.svelte";
+</script>
+
+<Header/>
+
+<div>
+    <h2>Comunícate con nosotros </h2>
+    <form action="" method="post">
+        <div>
+            <Input/>
+            <Input/>
+        </div>
+        <Input/>
+        <button type="submit">Enviar</button>
+    </form>
+</div>
+
+<div>
+    <h1>Puedes encontrarnos en Cra. 47 #50-75 Loca 105 </h1>
+    <div>
+        <iframe allowfullscreen=""
+                height="450" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d360.78052336330694!2d-75.56628653708843!3d6.2493650725370795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e4428f8896a73f3%3A0xc3a30926a6f6286d!2sPanader%C3%ADa%20y%20Pasteler%C3%ADa%20Astoria!5e0!3m2!1ses-419!2sco!4v1692655167350!5m2!1ses-419!2sco"
+                style="border:0;"
+                width="600"></iframe>
+    </div>
+</div>
+
+<div>
+    <h2>
+        Puedes encontrarnos en tus redes sociales
+    </h2>
+    <div>
+        <ButtonSocial/>
+        <ButtonSocial/>
+        <ButtonSocial/>
+    </div>
+</div>
+
+<Footer/>
