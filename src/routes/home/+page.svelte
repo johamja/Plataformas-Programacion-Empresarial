ef97f27c1fe78a3638aa9da696366e06cb7f1f74
Joham Jaider Morales Franco <johamfranco318@gmail.com>
2023-08-21 18:13:08 -0500

    add structure for home

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+    import Header from "../../components/Header.svelte";
+    import Footer from "../../components/Footer.svelte";
+
+    import Product from "../../components/Product.svelte";
+    import Description from "../../components/Description.svelte";
+</script>
+
+<Header/>
+
+<div>
+    <h1>Panyucando</h1>
+    <h3>Sabor Auténtico en Cada Creación</h3>
+</div>
+
+<div>
+    <h2>Productos</h2>
+    <div>
+        <Product/>
+        <Product/>
+        <Product/>
+    </div>
+</div>
+
+<div>
+    <h2>Nosotros te ofrecemos</h2>
+    <div id="conteiner">
+        <Description/>
+        <Description/>
+        <Description/>
+        <Description/>
+        <Description/>
+        <Description/>
+    </div>
+</div>
+
+<div>
+    <h2>¿Cómo comprar?</h2>
+</div>
+
+<Footer/>
