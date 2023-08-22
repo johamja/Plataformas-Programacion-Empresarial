1356b4cf80d19d4c4f7e2017a4fc99a415380347
Joham Jaider Morales Franco <johamfranco318@gmail.com>
2023-08-22 17:25:10 -0500

    update component Product.svelte

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -1,5 +1,36 @@
+<script>
+    export let img
+    let src = img.toString()
+    export let Name
+    let name = Name.toString()
+    export let Description
+    let description = Description.toString()
+</script>
+
 <div>
-    <img src="" alt="">
-    <h2></h2>
-    <p></p>
+    <img src="{src}" alt="img_product">
+    <h2>{name}</h2>
+    <p>{description}</p>
 </div>
+
+<style>
+    div{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 15%;
+    }
+ img{
+     --tamaño: 80%;
+     width: var(--tamaño);
+ }
+ h2, p{
+    font-weight: normal;
+ }
+ h2{
+     text-align: center;
+ }
+ p{
+     text-align: justify;
+ }
+</style>
