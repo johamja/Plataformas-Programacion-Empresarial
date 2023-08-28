aa06037bcd2dcb24eb588c376207872a11b8e746
Joham Jaider Morales Franco <johamfranco318@gmail.com>
2023-08-28 18:10:26 -0500

    updated the store

---
+++ src/components/Products_list.svelte
@@ -0,0 +1,107 @@
+<script>
+
+    export let Item
+
+    let name = Item.name
+    let price = Item.price
+    let img = Item.img
+    let status = Item.status
+    let discount = Item.discount
+
+
+    let onclick = true
+
+    function OnClick(){
+        onclick = !onclick
+    }
+
+</script>
+
+<div class="component">
+    <div class="container_1">
+        <div class="background_1">
+                <div class="sub_background">
+                    <div class="status indicator">
+                        <p>{status}</p>
+                    </div>
+                    <div class="discount indicator">
+                        <p>% {discount}</p>
+                    </div>
+                    <img src="{img}" alt="">
+                </div>
+        </div>
+        <div class="background_2">
+            <p class="name">{name}</p>
+            <p class="price">$ {price}</p>
+            <button class="add">Añadir</button>
+        </div>
+    </div>
+</div>
+
+<style>
+    .component{
+        width: 290px;
+        height: 480px;
+    }
+    .container_1{
+        width: auto;
+        height: auto;
+        display: flex;
+        flex-direction: column;
+        margin: 15px;
+    }
+
+    .background_1{
+        border-radius: 10px;
+        background: var(--color-C);
+        & .sub_background{
+            display: flex;
+            flex-direction: column;
+            margin: 12px;
+            & .status{
+                background-color: var(--color-D);
+                margin-bottom: 8px;
+            }
+            & .discount{
+                background-color: var(--color-A);
+            }
+        }
+        & img {
+            height: 100%;
+        }
+    }
+    .indicator{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 10px;
+        width: 83px;
+        height: 34px;
+        & p{
+            text-align: center;
+            margin: 0;
+            color: var(--color-B);
+            font-weight: normal;
+            font-size: 15px;
+        }
+    }
+    .background_2{
+        & p{
+            margin: 7px;
+        }
+        & .price{
+            font-weight: bold;
+        }
+        & .add{
+            background-color: var(--color-A);
+            color: var(--color-B);
+            width: 100%;
+            height: 40px;
+            border-radius: 10px;
+            border-block-start: none;
+            border-block-end: none;
+            border-inline-end: none;
+            border-inline-start: none;
+        }
+    }
+</style>
--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,8 +1,268 @@
+<script>
+    // import from components
+    import Space from '../../components/design/space.svelte'
+    import Products_list from "../../components/Products_list.svelte";
+
+    // import from styles
+    import '../../styles/color.css'
+
+    // import from lib/products
+    import img_example from '$lib/img/product/prod_1.png'
+
+    // list the products
+    let list_products_stock = [
+        {
+            name: 'Pan de yuca',
+            price: 12000,
+            img: img_example,
+            status: 'Nuevo',
+            discount: 5
+        },
+        {
+            name: 'torta',
+            price: 212,
+            img: img_example,
+            status: 'Nuevo',
+            discount: 10
+        },
+        {
+            name: 'biscocho',
+            price: 212,
+            img: img_example,
+            status: 'Nuevo',
+            discount: 8
+        },
+        {
+            name: 'Pan de yuca',
+            price: 12000,
+            img: img_example,
+            status: 'Nuevo',
+            discount: 5
+        },
+        {
+            name: 'torta',
+            price: 212,
+            img: img_example,
+            status: 'Nuevo',
+            discount: 10
+        },
+        {
+            name: 'biscocho',
+            price: 212,
+            img: img_example,
+            status: 'Nuevo',
+            discount: 8
+        },
+        {
+            name: 'Pan de yuca',
+            price: 12000,
+            img: img_example,
+            status: 'Nuevo',
+            discount: 5
+        },
+        {
+            name: 'torta',
+            price: 212,
+            img: img_example,
+            status: 'Nuevo',
+            discount: 10
+        },
+        {
+            name: 'biscocho',
+            price: 212,
+            img: img_example,
+            status: 'Nuevo',
+            discount: 8
+        }
+]
+
+    let list_products_out_stock = [
+        {
+            name: 'Pan de yuca',
+            price: 12000,
+            img: img_example,
+            status: 'Nuevo',
+            discount: 5
+        },
+        {
+            name: 'torta',
+            price: 212,
+            img: img_example,
+            status: 'Nuevo',
+            discount: 10
+        },
+        {
+            name: 'biscocho',
+            price: 212,
+            img: img_example,
+            status: 'Nuevo',
+            discount: 8
+        }
+    ]
+</script>
+
 <svelte:head>
     <title>Shop</title>
-    <meta name="description" content="Description" />
+    <meta content="Description" name="description"/>
 </svelte:head>
 
 <div>
-    <h1> Hola </h1>
+
+    <Space/>
+
+    <div class="container_1 flex_center">
+        <div class="sub_container_1">
+            <div class="publisher_1 publisher_root">
+            </div>
+            <div class="publisher_2 publisher_root"></div>
+            <div class="publisher_3 publisher_root"></div>
+            <div class="publisher_4 publisher_root"></div>
+        </div>
+    </div>
+
+    <Space/>
+
+    <div class="container_2">
+        <div class="sub_container_1">
+            <h2>los recién llegados </h2>
+            <div class="list_products_stock">
+                {#each list_products_stock as item}
+                    <Products_list Item={item}/>
+                {/each}
+            </div>
+        </div>
+    </div>
+
+    <Space/>
+
+    <div class="container_3">
+        <div class="sub_container_1">
+            <div class="publisher_1 publisher_root"></div>
+        </div>
+    </div>
+
+    <Space/>
+
+    <div class="container_4">
+        <div class="sub_container_1">
+            <h2>Si unidades para la venta</h2>
+            <div class="list_products_stock">
+                {#each list_products_out_stock as item}
+                    <Products_list Item={item}/>
+                {/each}
+            </div>
+        </div>
+    </div>
+
+    <Space/>
+
+    <div class="container_5">
+        <div class="sub_container_1">
+            <div class="publisher_1 publisher_root"></div>
+        </div>
+    </div>
+    <Space/>
 </div>
+
+<style>
+    .flex_center {
+        display: flex;
+        justify-content: space-evenly;
+    }
+
+    .publisher_root {
+        background: var(--color-C);
+        border-radius: 10px;
+    }
+
+    .container_1 {
+        & .sub_container_1 {
+            display: grid;
+            grid-template-columns: repeat(4, auto);
+            grid-template-rows: repeat(2, auto);
+            width: 80%;
+            height: 470px;
+            grid-column-gap: 10px;
+            grid-row-gap: 10px;
+            & .publisher_1 {
+                grid-column: 1/3;
+                grid-row: 1/3;
+            }
+
+            & .publisher_2 {
+                grid-column: 3/5;
+                grid-row: 1;
+            }
+
+            & .publisher_3 {
+                grid-column: 3;
+                grid-row: 2;
+            }
+
+            & .publisher_4 {
+                grid-column: 4;
+                grid-row: 2;
+            }
+        }
+
+    }
+
+    .container_2 {
+        margin: 0 var(--padding-page) 0 var(--padding-page);
+        & .sub_container_1 {
+            & .list_products_stock{
+                display: flex;
+                flex-direction: row;
+                flex-wrap: wrap;
+                justify-content: space-evenly;
+            }
+            & h2{
+                text-align: center;
+                font-weight: normal;
+                font-size: 40px;
+            }
+        }
+
+    }
+
+    .container_3 {
+        margin: 0 var(--padding-page) 0 var(--padding-page);
+        & .sub_container_1{
+            & .publisher_1{
+                width: 100%;
+                height: 500px;
+                background-color: var(--color-C);
+            }
+        }
+    }
+
+    .container_4 {
+        margin: 0 var(--padding-page) 0 var(--padding-page);
+        & .sub_container_1 {
+            & .list_products_stock{
+                display: flex;
+                flex-direction: row;
+                flex-wrap: wrap;
+                justify-content: space-evenly;
+            }
+            & h2{
+                text-align: center;
+                font-weight: normal;
+                font-size: 40px;
+            }
+        }
+
+    }
+
+    .container_5 {
+        margin: 0 var(--padding-page) 0 var(--padding-page);
+        & .sub_container_1{
+            & .publisher_1{
+                width: 100%;
+                height: 500px;
+                background-color: var(--color-C);
+            }
+        }
+    }
+
+</style>
